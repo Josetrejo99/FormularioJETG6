505fcbe9d6bcc8394f5723ecf45b8ce2f7db3798
Josetrejo99 <63658617+Josetrejo99@users.noreply.github.com>
2020-07-06 09:39:50 -0400

    mi primer commit

---
+++ cadenas/index.php
@@ -0,0 +1,27 @@
+<?php
+
+echo "Ejercicios de cadenas ";echo "<br><br>";
+echo "Uso de la funcion substr para quitar caracteres de una cadena";
+echo "<br><br>";
+echo "Ejercicio 1";echo "<br><br>";
+
+$frase_1=substr("La inteligencia artificial",3,18);
+
+echo "Frase 1: ".$frase_1."<br><br>";
+
+echo "Ejercicio 2";echo "<br><br>";
+
+$frase_2=substr($frase_1,0,8);
+
+echo "Frase 2: ".$frase_2."<br><br>";
+
+
+
+
+
+
+
+
+
+
+?>
